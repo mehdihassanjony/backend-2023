990bb0ecb3ecc69f0ecd12d03061e1ad54f1d402
Mehdi Hassan Jony <shoyshab@gmail.com>
2023-01-29 12:22:00 +0600

    database updated

--- prisma/migrations/20230128063829_backend/migration.sql
+++ prisma/migrations/20230128063829_backend/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE "Product" (
     "price" DOUBLE PRECISION NOT NULL,
     "stocks" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
